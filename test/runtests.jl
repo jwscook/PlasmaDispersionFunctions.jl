5d90fb4097c8411a036eee71d38d80aedfe599a3
James Cook <cookjws@gmail.com>
2023-07-24 19:17:02 +0100

    add tests to compare

--- test/runtests.jl
+++ test/runtests.jl
@@ -63,22 +63,44 @@ end
 @testset "check errors are caught" begin
   @test_throws ArgumentError plasmadispersionfunction(1.0, -1)
 end
-gpdr = generalisedplasmadispersionfunction
-pdr = plasmadispersionfunction
+gpdf = generalisedplasmadispersionfunction
+pdf = plasmadispersionfunction
 @testset "Genearlised plasma dispersion function" begin
   maxwellian(x) = exp(-x^2) / sqrt(π)
-  @test gpdr(maxwellian, 0.0) ≈ im*sqrt(pi) rtol=1e-5
-  @test gpdr(maxwellian, im) ≈ im*0.757872156141312 rtol=1e-5
-  @test gpdr(maxwellian, ComplexF64(-1.52, 0.47), 0) ≈
+  @test gpdf(maxwellian, 0.0) ≈ im*sqrt(pi) rtol=1e-5
+  @test gpdf(maxwellian, im) ≈ im*0.757872156141312 rtol=1e-5
+  @test gpdf(maxwellian, ComplexF64(-1.52, 0.47), 0) ≈
     ComplexF64(0.6088888957234254, 0.33494583882874024) rtol=1e-5
-  @test gpdr(maxwellian, big(0.0+0im)) ≈
+  @test gpdf(maxwellian, big(0.0+0im)) ≈
     ArbComplex(im*sqrt(pi)) rtol=1e-5
   for i in (10 .^(0.5:0.5:16)) * eps()
-    @test gpdr(maxwellian, 1.0+ im*i) ≈ pdr(1.0+im*i)
-    @test gpdr(maxwellian, 1.0- im*i) ≈ pdr(1.0-im*i)
+    @test gpdf(maxwellian, 1.0+ im*i) ≈ pdf(1.0+im*i)
+    @test gpdf(maxwellian, 1.0- im*i) ≈ pdf(1.0-im*i)
   end
 end
 
+@testset "use from SI units" begin
+  vth = 1e6 * rand()
+  f(v) = exp(-(v/vth)^2) / sqrt(pi) / vth
+  @testset "simple maxwellian forwards" begin
+    @assert quadgk(f, -6vth, 6vth)[1] ≈ 1
+    z = 1.0
+    @test pdf(z) ≈ gpdf(f, z * vth) * vth
+  end
 
+  @testset "simple maxwellian backwards" begin
+    # need to turn exp(-v^2/vth^2) into exp(-x^2) otherwise we can't compare with 
+    # the standard pdf
+    vnorm = vth
+    fnorm(x) = f(x * vnorm) * vnorm
+    vthnorm = vth / vnorm
+    @assert quadgk(fnorm, -6vthnorm, 6vthnorm)[1] ≈ 1
+    z = vnorm * (1 + im/100) * rand()
+    znorm = z / vnorm
+    expected = pdf(znorm)
+    result = gpdf(fnorm, znorm)
+    @test result ≈ expected
+  end
+end
 
 end
